6609de207a8870fc55b71c52fa5a665b7e7aea44
techzik323 <techzik323@gmail.com>
2024-12-11 23:34:37 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ const fs = require('fs')
 const { color } = require('./lib/color')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 //=================================================//
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'XPLOADER~PvBniCjR#TKNl_ncqKfJirqVnkamy7oZxjp3_lgHLNqD73rDsTbo' 
 //Enter your Xploader session id here; must start with XPLOADER~
 
 //=================================================//
